ef663f951beb19ac3abf57082432fd1c1fbba776
David222 <yerssondavidf@gmail.com>
2026-09-15 12:13:10 -0500

    feat: initialize Spring Boot backend project

---
+++ backend/src/main/java/com/nocountry/simulation/communitylab/CommunityLabApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.simulation.communitylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityLabApplication.class, args);
+    }
+
+}
